bae2e4aa5c8e3e60127303fdfc30680303d27827
somu3720 <somu3720@gmail.com>
2021-09-07 15:19:41 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,40 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+
+public class Test
+{ 
+    public static int countDigits(int[] a,int n){
+        int count=0;boolean isDistinct=true;
+        for(int i=0;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(a[i]==a[j]){
+                    isDistinct=false;
+                    break;
+                }
+            }
+            if(isDistinct==true){
+                count++;
+            }
+            isDistinct=true;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        System.out.print(countDigits(a,n));
+    }
+       
+} 
+
+
+
+
+
